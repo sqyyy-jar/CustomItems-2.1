97ebbdba35f4babc781dac23f3a493b49f8aa14a
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-18 18:44:23 +0200

    Update InventoryCloseListener.java

--- src/main/java/dev/sqyyy/customitems/ii/gui/listeners/InventoryCloseListener.java
+++ src/main/java/dev/sqyyy/customitems/ii/gui/listeners/InventoryCloseListener.java
@@ -1,5 +1,7 @@
 package dev.sqyyy.customitems.ii.gui.listeners;
 
+import dev.sqyyy.customitems.ii.Main;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
@@ -7,6 +9,6 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 public class InventoryCloseListener implements Listener {
     @EventHandler
     public void onClose(InventoryCloseEvent e) {
-
+        Main.getPlugin().getGuiManager().close((Player) e.getPlayer());
     }
 }
