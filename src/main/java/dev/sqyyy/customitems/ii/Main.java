ae3fbf6ac397f1f1ff1dd3021cd0cbdf2da52e0d
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-18 13:28:01 +0200

    Update Main.java
    cleared Main

--- src/main/java/dev/sqyyy/customitems/ii/Main.java
+++ src/main/java/dev/sqyyy/customitems/ii/Main.java
@@ -5,13 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Main extends JavaPlugin {
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
+    public void onEnable() {}
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+    public void onDisable() {}
 }
