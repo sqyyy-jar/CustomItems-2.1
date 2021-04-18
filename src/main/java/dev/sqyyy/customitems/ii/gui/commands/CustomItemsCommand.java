4f3fad297d796772ef55c8a7e68201313b13a57f
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-18 18:44:17 +0200

    Update CustomItemsCommand.java

--- src/main/java/dev/sqyyy/customitems/ii/gui/commands/CustomItemsCommand.java
+++ src/main/java/dev/sqyyy/customitems/ii/gui/commands/CustomItemsCommand.java
@@ -1,13 +1,24 @@
 package dev.sqyyy.customitems.ii.gui.commands;
 
+import dev.sqyyy.customitems.ii.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class CustomItemsCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        return false;
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("§c§lYou have to be a player to execute this command!");
+            return true;
+        }
+        if (args.length != 0) {
+            return false;
+        }
+        Player p = (Player) sender;
+        Main.getPlugin().getGuiManager().open(p);
+        return true;
     }
 }
