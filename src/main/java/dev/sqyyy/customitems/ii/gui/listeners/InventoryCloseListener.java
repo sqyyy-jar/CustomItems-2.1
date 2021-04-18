dff20517a50d0c9f2fbfa2eae243b9e72cceac80
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-18 13:50:19 +0200

    Create InventoryCloseListener.java

---
+++ src/main/java/dev/sqyyy/customitems/ii/gui/listeners/InventoryCloseListener.java
@@ -0,0 +1,12 @@
+package dev.sqyyy.customitems.ii.gui.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+public class InventoryCloseListener implements Listener {
+    @EventHandler
+    public void onClose(InventoryCloseEvent e) {
+
+    }
+}
