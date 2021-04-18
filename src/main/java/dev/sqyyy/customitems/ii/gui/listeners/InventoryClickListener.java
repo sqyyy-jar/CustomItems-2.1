b094799e5ad1bf8931e4aa839be7b11957876852
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-18 13:50:22 +0200

    Create InventoryClickListener.java

---
+++ src/main/java/dev/sqyyy/customitems/ii/gui/listeners/InventoryClickListener.java
@@ -0,0 +1,12 @@
+package dev.sqyyy.customitems.ii.gui.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class InventoryClickListener implements Listener {
+    @EventHandler
+    public void onClick(InventoryClickEvent e) {
+
+    }
+}
