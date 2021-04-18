5d56c937554874ce3e336859e3d224ac98516353
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-18 13:50:26 +0200

    Create CustomItemsCommand.java

---
+++ src/main/java/dev/sqyyy/customitems/ii/gui/commands/CustomItemsCommand.java
@@ -0,0 +1,13 @@
+package dev.sqyyy.customitems.ii.gui.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class CustomItemsCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return false;
+    }
+}
