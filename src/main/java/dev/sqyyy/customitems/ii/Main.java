c2824deeb17da10db069ff9170d005161f45cf65
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-18 13:49:37 +0200

    Update Main.java

--- src/main/java/dev/sqyyy/customitems/ii/Main.java
+++ src/main/java/dev/sqyyy/customitems/ii/Main.java
@@ -1,12 +1,26 @@
 package dev.sqyyy.customitems.ii;
 
+import dev.sqyyy.customitems.ii.gui.GuiManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+    private static Main pl;
+    private GuiManager guiManager;
 
     @Override
-    public void onEnable() {}
+    public void onEnable() {
+        pl = this;
+        this.guiManager = new GuiManager();
+    }
 
     @Override
     public void onDisable() {}
+
+    public static Main getPlugin() {
+        return pl;
+    }
+
+    public GuiManager getGuiManager() {
+        return guiManager;
+    }
 }
