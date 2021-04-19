81d6c30c040a57c4e5819d41f6dca10e13f18a21
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-04-19 17:59:16 +0200

    Update Main.java

--- src/main/java/dev/sqyyy/customitems/ii/Main.java
+++ src/main/java/dev/sqyyy/customitems/ii/Main.java
@@ -1,6 +1,8 @@
 package dev.sqyyy.customitems.ii;
 
 import dev.sqyyy.customitems.ii.gui.GuiManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -14,7 +16,12 @@ public final class Main extends JavaPlugin {
     }
 
     @Override
-    public void onDisable() {}
+    public void onDisable() {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            p.closeInventory();
+            guiManager.close(p);
+        }
+    }
 
     public static Main getPlugin() {
         return pl;
